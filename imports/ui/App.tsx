79a098b4361983fcae85639d79a6e2be84fb54a5
Robert Sherling <5183016+Rob117@users.noreply.github.com>
2022-03-25 13:34:57 +0900

    basic client-side count functionality

--- imports/ui/App.tsx
+++ imports/ui/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { ChangeCountButton } from './components/changeCountButton';
+import { useChangeIncrement } from './hooks/useChangeIncrement';
 
-export const App = () => (
-  <div>
-    <h1>Time To Count</h1>
-
-    <ChangeCountButton label='countUp' onClick={() => {}}/>
-  </div>
-);
+export const App = () => {
+  const { count, incrementCount, decrementCount } = useChangeIncrement();
+  return (
+    <div>
+      <h1>Time To Count</h1>
+      {count}
+      <div>
+        <ChangeCountButton label='count up' onClick={incrementCount}/>
+        <ChangeCountButton label='count down' onClick={decrementCount}/>
+      </div>
+    </div>
+  );
+}
--- imports/ui/hooks/useChangeIncrement.tsx
+++ imports/ui/hooks/useChangeIncrement.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const useChangeIncrement = () => {
-  return (
-    <div>useChangeIncrement</div>
-  )
+  // track count, offer count, increment, decrement functions
+  const [count, setCount] = useState(0);
+  
+  const incrementCount = () => {
+    setCount(count + 1);
+  }
+
+  const decrementCount = () => {
+    setCount(count - 1);
+  }
+  
+  return {
+    count,
+    incrementCount,
+    decrementCount
+  }
 }
