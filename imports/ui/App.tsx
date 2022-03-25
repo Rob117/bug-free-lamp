5765454bd17fe78255c0820bb41d262398dfc0cd
Robert Sherling <5183016+Rob117@users.noreply.github.com>
2022-03-25 13:50:40 +0900

    Added basic mailto, see notes
    If you are having trouble getting the email link to work (I did), go into your browser setting and confirm that email handlers are not blocked.
    
    Then go to your email client and enable it as a default handler.
    
    I had no handler enabled - this took a while to troubleshoot.

--- imports/ui/App.tsx
+++ imports/ui/App.tsx
@@ -4,6 +4,10 @@ import { useChangeIncrement } from './hooks/useChangeIncrement';
 
 export const App = () => {
   const { count, incrementCount, decrementCount } = useChangeIncrement();
+
+  const clickMailTo = () => {
+    window.open(`mailto:?subject=Your Count&body=${count}`)
+  }
   return (
     <div>
       <h1>Time To Count</h1>
@@ -12,6 +16,11 @@ export const App = () => {
         <ChangeCountButton label='count up' onClick={incrementCount}/>
         <ChangeCountButton label='count down' onClick={decrementCount}/>
       </div>
+      <br/>
+      <br/>
+      <button onClick={clickMailTo}>
+        Send Email
+      </button>
     </div>
   );
 }
